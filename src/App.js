91e03701588431682ff4d8689c10bcbbb8a98d58
kokseng1 <ngkokseng02@gmail.com>
2023-05-19 13:59:58 +0800

    added Options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Options from './Components/Options';
 
 function App() {
   return (
     <div className="App">
+      <h1>
       Project Wander
+      </h1>
+      <Options />
     </div>
   );
 }
---
+++ src/Components/Options.js
@@ -0,0 +1,12 @@
+export default function Options() {
+    return (
+    <>
+    <p>
+        <a href="test.com">Option 1</a>
+    </p>
+    <p>
+        <a href="test.com">Option 2</a>
+    </p>
+    </>
+    );
+}
