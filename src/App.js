1e7e272ee95c668a1044f31856381fa72217c9d0
kokseng1 <ngkokseng02@gmail.com>
2023-05-30 15:12:13 +0800

    added story Text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Options from './Components/Options';
+import Text from './Components/Options';
 
 function App() {
   return (
     <div className="App">
       <h1 class = "header">
-      Project Wander
+      Project Wander2
       </h1>
+      <Options/>
+      div
       <Options />
     </div>
   );
---
+++ src/Components/Text.js
@@ -0,0 +1,8 @@
+export default function Text() {
+    return (
+    <h2>
+        You arrive at the extrance of the maze empty handed. A maze warden stands in front of an open gate, 
+        he does not say a word, but reaches his hand out to offer you a staff. It looks like an amateur's staff they use at training schools. 
+    </h2>
+    );
+}
