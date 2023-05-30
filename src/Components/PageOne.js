a9998b8a6c77f0cd37c1826da2e61ed41615ff96
kokseng1 <ngkokseng02@gmail.com>
2023-05-30 17:52:40 +0800

    Added first choice interation

---
+++ src/Components/PageOne.js
@@ -0,0 +1,12 @@
+export default function PageOne() {
+    return (
+    <div >
+    <p>
+        <a href="test.com" >Accept the staff</a>
+    </p>
+    <p>
+        <a href="test.com">Reject the staff</a>
+    </p>
+    </div>
+    );
+}
