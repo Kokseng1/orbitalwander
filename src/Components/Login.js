d4499af285da2b7602af2126471715cd16818fdb
austi <austinhuang1203@gmail.com>
2023-07-23 20:43:38 +0800

    some config info

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,6 +3,14 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
 import { Link } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import firebaseConfig from "../firebaseConfig"; // Adjust the path accordingly
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -12,9 +20,29 @@ export default function Login() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+  
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      // Handle successful login, e.g., redirect to a dashboard page
+    } catch (error) {
+      // If login fails with 'auth/user-not-found' error, create a new account
+      if (error.code === 'auth/user-not-found') {
+        try {
+          await firebase.auth().createUserWithEmailAndPassword(email, password);
+          // Handle successful account creation, e.g., redirect to a welcome page
+        } catch (error) {
+          // Handle account creation error, e.g., display an error message
+          console.error("Error creating account: ", error);
+        }
+      } else {
+        // Handle other login errors
+        console.error("Error logging in: ", error);
+      }
+    }
   }
+  
 
   return (
     <div className="Login">
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzsvAii7nn2jWAwhzomPurcOmgETeJt2I",
+    authDomain: "orbitalwander1.firebaseapp.com",
+    projectId: "orbitalwander1",
+    storageBucket: "orbitalwander1.appspot.com",
+    messagingSenderId: "343438217401",
+    appId: "1:343438217401:web:028a4c74614e6a74218b68"
+  };
+  
+  export default firebaseConfig;
+  
